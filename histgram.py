c34ae7a41e3ffad2ee98582d58d6f208d561b82e
HiroakiOkada <64031703+Hiroaki-Okada@users.noreply.github.com>
2023-05-15 19:31:37 +0900

    Update histgram.py

--- histgram.py
+++ histgram.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 
 
 def plot_histgram(x):
-    plt.figure(figsize=(12, 12))
-    plt.hist(x, bins=10, color='darkgrey')
-    plt.xlabel('x', fontsize=30)
-    plt.ylabel('Number', fontsize=30)
-    plt.tick_params(labelsize=30)
+    plt.figure(figsize=(15, 15))
+    plt.hist(x, bins=15, color='darkgrey')
+    plt.xlabel('Experimental selectivity (%)', fontsize=50, labelpad=10)
+    plt.ylabel('Number', fontsize=50)
+    plt.xlim(0, 100)
+    plt.tick_params(labelsize=40)
     plt.tight_layout()
     plt.savefig('Histgram.jpeg')
 
 
 def run():
     print('Enter input_name (xxx.xlsx or xxx.csv)')
-    input_name = input()
+    # input_name = input()
+    input_name  = 'experimental_selectivity.xlsx'
 
     if 'xlsx' in input_name:
         df = pd.read_excel(input_name)
